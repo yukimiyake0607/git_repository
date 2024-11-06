829909e1bb744031974b22ff4ecc3317f535a07e
yukimiyake0607 <yukimiyake38@gmail.com>
2024-11-06 11:24:27 +0900

    rename: 管理しているフォルダの名前を変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:git_repository/presentation/app.dart';
+import 'package:git_repository/app/app.dart';
 
 void main() {
   runApp(const MainApp());
