7a0569e8ce6faa738bb00ec608efe486a5483559
yukimiyake0607 <yukimiyake38@gmail.com>
2024-11-08 22:50:16 +0900

    add: ProviderScopeを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:git_repository/app/app.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(ProviderScope(child: const MainApp()));
 }
