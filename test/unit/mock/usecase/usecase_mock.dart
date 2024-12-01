affbf1d84ef5fddaa0e4949d57b06c8f2f043010
yukimiyake0607 <yukimiyake38@gmail.com>
2024-12-01 23:34:50 +0900

    add: usecaseのMockを新規作成

---
+++ test/unit/mock/usecase/usecase_mock.dart
@@ -0,0 +1,6 @@
+import 'package:git_repository/data/usecase/repository_usecase.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<RepositoryUsecase>()])
+
+void main() {}
